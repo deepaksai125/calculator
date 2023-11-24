5886718197d02900938cf45ef43a7f3554b70deb
C.DEEPAK SAI <140964976+deepaksai125@users.noreply.github.com>
2023-11-24 20:47:23 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+let inputBox = document.getElementById('inputBox')
+let button = document.querySelectorAll('button')
+
+let string = ''
+
+button.forEach(element =>{
+  element.addEventListener('click', (b)=>{
+    if(b.target.innerText == '='){
+      string = String(eval(string))
+      inputBox.value = string;
+    }
+    else if (b.target.innerText == 'AC'){
+      string = ''
+      inputBox.value =string;
+    }
+    else if (b.target.innerText == 'DEL'){
+      string = string.substring(0,string.length-1)
+      inputBox.value = string;
+    }
+    else if(b.target.id == 'Pluseminse'){
+      string = String(-eval(string))
+      inputBox.value = string;
+      
+    }
+    else{
+      string += b.target.innerText
+      inputBox.value = string
+    }
+  })
+})
